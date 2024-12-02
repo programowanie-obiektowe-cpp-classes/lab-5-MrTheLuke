b90272c9c8d2f1cf9fe44654c640d91b81790690
Łukasz Spasiński <01171680@pw.edu.pl>
2024-12-02 09:08:33 +0100

    zejściówka laby 5

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    // Inkrementujemy wiek ka¿dej osoby w liœcie
+    for (auto& person : people) {
+        person.birthday();
+    }
+
+    // Tworzymy wektor wynikowy w odwrotnej kolejnoœci
+    std::vector< char > result;
+    for (auto it = people.rbegin(); it != people.rend(); ++it) {
+        // Dodajemy 'n', jeœli osoba jest "potworem", inaczej 'y'
+        if (it->isMonster()) {
+            result.push_back('n');
+        }
+        else {
+            result.push_back('y');
+        }
+    }
+
+    return result;
 }
